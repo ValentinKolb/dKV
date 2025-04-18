4f7e2748c4c3105cc7b798e91fa05925c2704d88
valentinkolb <valentin.kolb@uni-ulm.de>
2025-04-18 21:48:11 +0200

    feat(cli):
    added version and upgrade command

--- cmd/root.go
+++ cmd/root.go
@@ -1,12 +1,19 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/ValentinKolb/dKV/cmd/kv"
 	"github.com/ValentinKolb/dKV/cmd/lock"
 	"github.com/ValentinKolb/dKV/cmd/serve"
 	"github.com/ValentinKolb/dKV/cmd/util"
 	"github.com/spf13/cobra"
 	"os"
+	"os/exec"
+	"runtime"
+)
+
+const (
+	Version = "0.1.2"
 )
 
 var (
@@ -14,9 +21,76 @@ var (
 	// RootCmd represents the base command when called without any subcommands
 	RootCmd = &cobra.Command{
 		Use:   "dkv",
-		Short: "dKV is a dstore key-value store",
-		Long: `dKV is a dstore key-value store built on Dragonboat RAFT.
-This application can run as a server or client for interacting with the dKV store.`,
+		Short: "distributed key-value store",
+		Long: fmt.Sprintf(`dKV (v%s)
+
+A distributed, consistent key-value store library written in Go,
+leveraging RAFT consensus for linearizability and fault tolerance.`, Version),
+	}
+	versionCmd = &cobra.Command{
+		Use:   "version",
+		Short: "Print the version number of dKV",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Printf("dKV v%s\n", Version)
+		},
+	}
+
+	// upgradeCmd represents the upgrade command
+	upgradeCmd = &cobra.Command{
+		Use:   "upgrade",
+		Short: "Upgrade dKV to the latest version",
+		Long:  `Upgrade dKV to the latest version by downloading and running the installation script.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Upgrading dKV to the latest version...")
+
+			// Get installation path flag
+			installPath, _ := cmd.Flags().GetString("path")
+
+			// Get source flag
+			fromSource, _ := cmd.Flags().GetBool("source")
+
+			// Prepare command arguments
+			scriptURL := "https://raw.githubusercontent.com/ValentinKolb/dKV/refs/heads/main/install.sh"
+			var shellCmd *exec.Cmd
+
+			if runtime.GOOS == "windows" {
+				fmt.Println("Windows is not supported.")
+				os.Exit(1)
+			}
+
+			// Base command to download and execute the script
+			baseCmd := fmt.Sprintf("curl -s %s | bash", scriptURL)
+
+			// Add options if specified
+			options := ""
+			if installPath != "" {
+				options += fmt.Sprintf(" -- --path=%s", installPath)
+			}
+			if fromSource {
+				if options == "" {
+					options = " -- --source"
+				} else {
+					options += " --source"
+				}
+			}
+
+			// Combine the command
+			cmdStr := baseCmd + options
+
+			// Create and run the command
+			shellCmd = exec.Command("bash", "-c", cmdStr)
+			shellCmd.Stdout = os.Stdout
+			shellCmd.Stderr = os.Stderr
+
+			fmt.Println("Executing:", cmdStr)
+			err := shellCmd.Run()
+			if err != nil {
+				fmt.Printf("Error upgrading dKV: %v\n", err)
+				os.Exit(1)
+			}
+
+			fmt.Println("dKV has been successfully upgraded!")
+		},
 	}
 )
 
@@ -25,6 +99,12 @@ func init() {
 	RootCmd.AddCommand(serve.ServeCmd)
 	RootCmd.AddCommand(kv.KeyValueCommands)
 	RootCmd.AddCommand(lock.LockCommands)
+	RootCmd.AddCommand(versionCmd)
+	RootCmd.AddCommand(upgradeCmd)
+
+	// Add Flags for upgrade command
+	upgradeCmd.Flags().String("path", "", "Installation path for the upgraded version")
+	upgradeCmd.Flags().Bool("source", false, "Install from source instead of using pre-compiled binaries")
 
 	// Add Flags
 	key := "serializer"
